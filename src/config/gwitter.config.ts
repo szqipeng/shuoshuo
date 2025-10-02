b2794d07d377d4c809ffbe01ba5431986f10ea9a
szqp <szqp@foxmail.com>
2025-10-02 13:17:49 +0800

    更新

---
+++ src/config/gwitter.config.ts
@@ -0,0 +1,40 @@
+// Gwitter Configuration
+
+export interface GwitterConfig {
+  request: {
+    token: string;
+    clientID: string;
+    clientSecret: string;
+    owner: string;
+    repo: string;
+    pageSize?: number;
+    autoProxy?: string;
+  };
+  app?: {
+    onlyShowOwner?: boolean;
+    enableRepoSwitcher?: boolean;
+    enableAbout?: boolean;
+    enableEgg?: boolean;
+  };
+}
+
+// Default configuration
+export const config: GwitterConfig = {
+  request: {
+    token:
+      process.env.GITHUB_TOKEN,
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    pageSize: 6,
+    autoProxy:
+      'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
+    owner: 'SimonAKing',
+    repo: 'weibo',
+  },
+  app: {
+    onlyShowOwner: true,
+    enableAbout: true,
+    enableRepoSwitcher: false,
+    enableEgg: true,
+  },
+};
